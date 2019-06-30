0195780d30c259e62610eea5a67bc58f006b9f46
frainzy1477 <allsafe@my.com>
2019-06-30 13:49:52 +0800

    Remove duplicated directory

--- luci-app-clash/root/usr/share/clash/check_version.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ -f /usr/share/clash/new_version ];then
-rm -rf /usr/share/clash/new_version
-fi
-new_version=`wget -qO- "https://github.com/frainzy1477/clash/tags"| grep "/frainzy1477/clash/releases/tag/"| head -n 1| awk -F "/tag/v" '{print $2}'| sed 's/\">//'`
-echo $new_version > /usr/share/clash/new_version 2>&1 & >/dev/null
